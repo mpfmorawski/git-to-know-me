0095a376322fffe78aae3086b483a53a6c9a1cff
Paweł Kotiuk <kotiuk@zohomail.eu>
2022-04-19 18:12:00 +0200

    Create base for stats package

--- gtkm/main.py
+++ gtkm/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
 from auth.service_auth import auth
+from stats.stats_aggregator import stats
+from stats.github_fetcher import github_fetcher
 
 app = FastAPI()
 
 app.include_router(auth)
+app.include_router(stats)
+app.include_router(github_fetcher)
