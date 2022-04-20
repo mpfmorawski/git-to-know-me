5eaeb28b333ebcf1bfc7a10ad5a7a7f0d0b1a2e4
Paweł Kotiuk <kotiuk@zohomail.eu>
2022-04-20 16:04:03 +0200

    Allow serving static files with FastAPI

--- gtkm/main.py
+++ gtkm/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 
 from .auth.service_auth import auth
 from .stats.stats_aggregator import stats
@@ -9,3 +11,11 @@ app = FastAPI()
 app.include_router(auth)
 app.include_router(stats)
 app.include_router(github_fetcher)
+
+
+@app.get('/')
+async def redirect_typer():
+    return RedirectResponse("/index.html")
+
+
+app.mount("/", StaticFiles(directory="static"), name="static")
