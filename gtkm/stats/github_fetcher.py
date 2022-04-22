8fe0b20b322bb449e2ff9c2d40e600ccba1b95a3
Your Name <jaceman44@gmail.com>
2022-04-22 10:34:52 +0200

    Added simple https request to a GitHub API

--- gtkm/stats/github_fetcher.py
+++ gtkm/stats/github_fetcher.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from time import time
+import httpx
+import asyncio
 
 github_fetcher = APIRouter()
 
+async def task(URL):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(URL)
+        return response.text
 
-@github_fetcher.get("/github/stats/general_user")
-def get_general_stats():
-    return {"stars": 1, "github_url": "http://github.com/zdzisiek"}
+@github_fetcher.get("/github/stats/{user_name}")
+async def get_general_stats(git_user: str):
+    URL = "https://api.github.com/users/{}".format(git_user)
+    return await task(URL)
