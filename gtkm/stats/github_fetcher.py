1d0d31bb7f76f8a541251ff647d7cf399e518c52
Your Name <jaceman44@gmail.com>
2022-04-26 18:46:08 +0200

    Improved code readability

--- gtkm/stats/github_fetcher.py
+++ gtkm/stats/github_fetcher.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 import json as JSON
 import httpx
 
@@ -12,6 +13,7 @@ async def task(URL):
         response = await client.get(URL)
     return response.text
 
+
 def jsons_parser(basic_user_data: str, repos_info: str):
     JSON_basic_user_data = JSON.loads(basic_user_data)
     JSON_repos_info = JSON.loads(repos_info)
@@ -22,20 +24,22 @@ def jsons_parser(basic_user_data: str, repos_info: str):
         stargaze_count = stargaze_count+element.get("stargazers_count")
         forks_count = forks_count + element.get("forks_count")
 
+    ''' Fetched and parsed user data '''
     json_summary_file = {'name': JSON_basic_user_data.get("name").split()[0],
-                 'surname': JSON_basic_user_data.get("name").split()[1],
-                 'user_name': JSON_basic_user_data.get("login"),
-                 'stargaze_count': stargaze_count,
-                 'repos_count': len(JSON_repos_info),
-                 'forks_count': forks_count}
+                         'surname': JSON_basic_user_data.get("name").split()[1],
+                         'user_name': JSON_basic_user_data.get("login"),
+                         'stargaze_count': stargaze_count,
+                         'repos_count': len(JSON_repos_info),
+                         'forks_count': forks_count}
 
     return json_summary_file
 
 
 async def get_basic_info(git_user: str):
-    URL_basic_user_data = URL_BASE + "/users/{}".format(git_user)
+    URL_basic_user_data = URL_BASE + f"/users/{git_user}"
     basic_user_data = await task(URL_basic_user_data)
 
+    ''' Check if user exist, if not return error message'''
     try:
         JSON_temp_user_check = JSON.loads(basic_user_data)
         if JSON_temp_user_check.get("message") == "Not Found":
@@ -43,7 +47,7 @@ async def get_basic_info(git_user: str):
     except:
         pass
 
-    URL_repos_info = URL_BASE + "/users/{}/repos".format(git_user)
+    URL_repos_info = URL_BASE + f"/users/{git_user}/repos"
     repos_info = await task(URL_repos_info)
 
     ''' Return parsed user and repos info'''
@@ -51,5 +55,5 @@ async def get_basic_info(git_user: str):
 
 
 @github_fetcher.get("/github/stats/{user_name}")
-async def get_general_stats(git_user: str):
-    return await get_basic_info(git_user)
+async def get_general_stats_github(user_name: str) -> JSONResponse:
+    return JSONResponse(await get_basic_info(user_name))
--- gtkm/stats/stats_aggregator.py
+++ gtkm/stats/stats_aggregator.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+import json as JSON
+from .github_fetcher import get_general_stats_github
+
+URL = "http://127.0.0.1:8000/github/stats/{user_name}"
 
 stats = APIRouter()
 
-
-@stats.get("/api/stats/general_user/")
+@stats.get("/api/stats/general_user/{user_name}")
 def get_general_stats():
     return {"stars": 1, "github_url": "http://github.com/zdzisiek"}
