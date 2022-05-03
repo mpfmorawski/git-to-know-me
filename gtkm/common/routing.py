595a6b40ccfa43654808327fb9bccfe95acc298a
Paweł Kotiuk <kotiuk@zohomail.eu>
2022-05-03 23:07:27 +0200

    Assign port numbers to services

--- gtkm/common/routing.py
+++ gtkm/common/routing.py
@@ -1,3 +1,23 @@
+from strenum import StrEnum
+
+
+class Service(StrEnum):
+    ENDPOINT = "endpoint"  #endpoint used by user (access to site)
+    AUTH = 'auth'
+    GITHUB_FETCHER = 'github'
+    GITLAB_FETCHER = 'gitlab'
+    STATS_AGGREGATOR = 'aggregator'
+
+
+SERVICE_PORTS = {
+    Service.ENDPOINT: 8000,
+    Service.AUTH: 8001,
+    Service.GITHUB_FETCHER: 8002,
+    Service.GITLAB_FETCHER: 8003,
+    Service.STATS_AGGREGATOR: 8004
+}
+
+
 def gen_url(endpoint: str) -> str:
     '''Generates full URL from selected API endpoint'''
     return "http://127.0.0.1:8000" + endpoint
