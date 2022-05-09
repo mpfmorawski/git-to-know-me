27d099cb37cf9257d251d62fd7b9d753bc9ed7a0
Karol Niemczycki <01133121@pw.edu.pl>
2022-05-09 17:06:50 +0000

    Automated autoyapf fixes

--- gtkm/auth/database/database.py
+++ gtkm/auth/database/database.py
@@ -3,12 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = os.environ.get("AUTH_CONN_STRING", "sqlite:///./gtkm/auth/database/user_db.db")
+SQLALCHEMY_DATABASE_URL = os.environ.get(
+    "AUTH_CONN_STRING", "sqlite:///./gtkm/auth/database/user_db.db")
 
 if "AUTH_CONN_STRING" in os.environ.keys():
     engine = create_engine(SQLALCHEMY_DATABASE_URL)
 else:
-    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+    engine = create_engine(SQLALCHEMY_DATABASE_URL,
+                           connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
