fbe3024caf2b975a70880b85104efa5f30d6f359
Krzysztof Łapan <lapankrz@interia.pl>
2022-05-09 19:06:16 +0200

    Added Postgres DB support.

--- gtkm/auth/database/database.py
+++ gtkm/auth/database/database.py
@@ -1,11 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./gtkm/auth/database/user_db.db"
+SQLALCHEMY_DATABASE_URL = os.environ.get("AUTH_CONN_STRING", "sqlite:///./gtkm/auth/database/user_db.db")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL,
-                       connect_args={"check_same_thread": False})
+if "AUTH_CONN_STRING" in os.environ.keys():
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
