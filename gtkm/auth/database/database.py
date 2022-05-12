4695e3ae638541e85e18c9ef654200dd82ad1177
Karol Niemczycki <01133121@pw.edu.pl>
2022-05-12 16:42:09 +0000

    Automated autoyapf fixes

--- gtkm/auth/database/database.py
+++ gtkm/auth/database/database.py
@@ -5,14 +5,15 @@ import os
 
 DB_USERNAME = os.environ.get("DB_USERNAME", None)
 DB_PASSWORD = os.environ.get("DB_PASSWORD", None)
-DB_ADDRESS = os.environ.get("DB_ADDRESS", None) # hostname:port
+DB_ADDRESS = os.environ.get("DB_ADDRESS", None)  # hostname:port
 AUTH_DB_NAME = os.environ.get("AUTH_DB_NAME", None)
 if DB_USERNAME and DB_PASSWORD and DB_ADDRESS and AUTH_DB_NAME:
     SQLALCHEMY_DATABASE_URL = f'postgres://{DB_USERNAME}:{DB_PASSWORD}@{DB_ADDRESS}/{AUTH_DB_NAME}'
     engine = create_engine(SQLALCHEMY_DATABASE_URL)
 else:
     SQLALCHEMY_DATABASE_URL = "sqlite:///./gtkm/auth/database/user_db.db"
-    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+    engine = create_engine(SQLALCHEMY_DATABASE_URL,
+                           connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
