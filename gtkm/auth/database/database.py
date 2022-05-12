721953103df4365fd892732a47c3c0309a5cd530
Krzysztof Łapan <lapankrz@interia.pl>
2022-05-12 18:41:36 +0200

    Changed DB connection string creation.

--- gtkm/auth/database/database.py
+++ gtkm/auth/database/database.py
@@ -3,14 +3,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = os.environ.get(
-    "AUTH_CONN_STRING", "sqlite:///./gtkm/auth/database/user_db.db")
-
-if "AUTH_CONN_STRING" in os.environ.keys():
+DB_USERNAME = os.environ.get("DB_USERNAME", None)
+DB_PASSWORD = os.environ.get("DB_PASSWORD", None)
+DB_ADDRESS = os.environ.get("DB_ADDRESS", None) # hostname:port
+AUTH_DB_NAME = os.environ.get("AUTH_DB_NAME", None)
+if DB_USERNAME and DB_PASSWORD and DB_ADDRESS and AUTH_DB_NAME:
+    SQLALCHEMY_DATABASE_URL = f'postgres://{DB_USERNAME}:{DB_PASSWORD}@{DB_ADDRESS}/{AUTH_DB_NAME}'
     engine = create_engine(SQLALCHEMY_DATABASE_URL)
 else:
-    engine = create_engine(SQLALCHEMY_DATABASE_URL,
-                           connect_args={"check_same_thread": False})
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./gtkm/auth/database/user_db.db"
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
